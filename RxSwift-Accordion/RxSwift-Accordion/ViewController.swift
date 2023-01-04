10809d86c2758e16ac8eca0d759919d18b9d62ee
osmnnyldzz <osmanyildiz046@gmail.com>
2023-01-04 21:16:42 +0300

    dummyArray changed to BehaviorRelay array. dummyCategories created.

--- RxSwift-Accordion/RxSwift-Accordion/ViewController.swift
+++ RxSwift-Accordion/RxSwift-Accordion/ViewController.swift
@@ -15,20 +15,55 @@ class ViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     
-    private let dummyArray = ["Lorem","Lorem","Lorem"]
+    private let categories: BehaviorRelay<[Categories]> = BehaviorRelay(value: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        Observable.of(self.dummyArray).asObservable()
+        self.createDummyCategories()
+        self.bindTableView()
+        
+    }
+    
+    private func bindTableView() {
+        self.categories.asObservable()
             .bind(to: tableView.rx
                 .items(cellIdentifier: MyTableViewCell.identifier, cellType: MyTableViewCell.self))
             { index, element, cell in
                 
-            cell.titleLabel.text = element
+                cell.titleLabel.text = element.categoryName
                 
         }.disposed(by: disposeBag)
+    }
+    
+    private func createDummyCategories() {
+        var dummyCategories:[Categories] = []
         
+        for category in 0...10 {
+            let dummyCategory = Categories(categoryName: "Category \(category)",
+                                           expand: false,
+                                           level: 0,
+                                           canExpand:  category % 2 == 0 ? true : false,
+                                           children: [])
+            dummyCategories.append(dummyCategory)
+            self.categories.accept(dummyCategories)
+        }
+    }
+}
+
+private class Categories {
+    var categoryName:String
+    var expand:Bool
+    var level:Int
+    var canExpand:Bool
+    var children:[Categories]
+    
+    init(categoryName: String, expand: Bool, level: Int, canExpand: Bool, children: [Categories]) {
+        self.categoryName = categoryName
+        self.expand = expand
+        self.level = level
+        self.canExpand = canExpand
+        self.children = children
     }
 }
 
